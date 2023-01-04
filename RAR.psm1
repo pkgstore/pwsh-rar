f7701d924364b47acae455848b9fa630c509706e
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-05 02:15:07 +0300

    2023-01-04 23:15:07

--- RAR.psm1
+++ RAR.psm1
@@ -23,42 +23,42 @@ function Compress-RAR() {
     [Parameter(Mandatory, HelpMessage="File list.")]
     [SupportsWildcards()]
     [Alias('File', 'F')]
-    [string[]]$Files,
+    [string[]]${Files},
 
     [Parameter(HelpMessage="Compression level. Value: [0 | ... | 5]. Default: 3.")]
     [ValidateRange(0,5)]
     [Alias('LVL', 'L')]
-    [int]$Level = 3,
+    [int]${Level} = 3,
 
     [Parameter(HelpMessage="Specify version of archiving format. Value: [4 | 5]. Default: 5.")]
     [ValidateRange(4,5)]
     [Alias('VER', 'V', 'MA')]
-    [int]$Version = 5,
+    [int]${Version} = 5,
 
     [Parameter(HelpMessage="Password. Encrypt both file data and headers.")]
     [Alias('PWD', 'P', 'HP')]
-    [string]$Password
+    [string]${Password}
   )
 
-  $RAR = "${PSScriptRoot}\Rar.exe"
-  $RAR_PF = "${ENV:ProgramFiles}\WinRAR\Rar.exe"
-  $RAR_PF86 = "${ENV:ProgramFiles(x86)}\WinRAR\Rar.exe"
+  ${RAR} = "${PSScriptRoot}\Rar.exe"
+  ${RAR_PF} = "${ENV:ProgramFiles}\WinRAR\Rar.exe"
+  ${RAR_PF86} = "${ENV:ProgramFiles(x86)}\WinRAR\Rar.exe"
 
   if ( Test-Path -Path "${RAR_PF}" -PathType "Leaf" ) {
-    $RAR = "${RAR_PF}"
+    ${RAR} = "${RAR_PF}"
   } elseif ( Test-Path -Path "${RAR_PF86}" -PathType "Leaf" ) {
-    $RAR = "${RAR_PF86}"
+    ${RAR} = "${RAR_PF86}"
   } else {
     Write-Error -Message "'Rar.exe' not found!" -ErrorAction "Stop"
   }
 
-  ForEach ( $File in ( Get-ChildItem ${Files} ) ) {
-    $FullName = "$( ${File}.FullName )"
-    $CMD = @( "a", "-m${Level}", "-ma${Version}" )
-    if ( -not ( [string]::IsNullOrEmpty(${Password}) ) ) { $CMD += "-hp${Password}" }
-    $CMD += @( "$( ${FullName} + '.rar' )", "${FullName}" )
+  ForEach ( ${File} in ( Get-ChildItem ${Files} ) ) {
+    ${FullName} = "$( ${File}.FullName )"
+    ${CMD} = @( "a", "-m${Level}", "-ma${Version}" )
+    if ( -not ( [string]::IsNullOrEmpty(${Password}) ) ) { ${CMD} += "-hp${Password}" }
+    ${CMD} += @( "$( ${FullName} + '.rar' )", "${FullName}" )
 
-    & "${RAR}" $CMD
+    & "${RAR}" ${CMD}
   }
 }
 
@@ -77,25 +77,25 @@ function Expand-RAR() {
     [Parameter(Mandatory, HelpMessage="File list.")]
     [SupportsWildcards()]
     [Alias('File', 'F')]
-    [string[]]$Files
+    [string[]]${Files}
   )
 
-  $RAR = "${PSScriptRoot}\Rar.exe"
-  $RAR_PF = "${ENV:ProgramFiles}\WinRAR\Rar.exe"
-  $RAR_PF86 = "${ENV:ProgramFiles(x86)}\WinRAR\Rar.exe"
+  ${RAR} = "${PSScriptRoot}\Rar.exe"
+  ${RAR_PF} = "${ENV:ProgramFiles}\WinRAR\Rar.exe"
+  ${RAR_PF86} = "${ENV:ProgramFiles(x86)}\WinRAR\Rar.exe"
 
   if ( Test-Path -Path "${RAR_PF}" -PathType "Leaf" ) {
-    $RAR = "${RAR_PF}"
+    ${RAR} = "${RAR_PF}"
   } elseif ( Test-Path -Path "${RAR_PF86}" -PathType "Leaf" ) {
-    $RAR = "${RAR_PF86}"
+    ${RAR} = "${RAR_PF86}"
   } else {
     Write-Error -Message "'Rar.exe' not found!" -ErrorAction "Stop"
   }
 
-  ForEach ( $File in ( Get-ChildItem "${Files}" ) ) {
-    $FullName = "$( ${File}.FullName )"
-    $CMD = @( "x", "${FullName}" )
+  ForEach ( ${File} in ( Get-ChildItem "${Files}" ) ) {
+    ${FullName} = "$( ${File}.FullName )"
+    ${CMD} = @( "x", "${FullName}" )
 
-    & "${RAR}" $CMD
+    & "${RAR}" ${CMD}
   }
 }
